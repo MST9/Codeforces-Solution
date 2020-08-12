62e2e857a5117dfa893902b646063da07ee83197
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2020-08-12 17:34:18 +0530

    Create 1185C1. Exam in BerSU (easy version).cpp

---
+++ 1185C1. Exam in BerSU (easy version).cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ff              first
+#define ss              second
+#define int             long long int
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int,vi,greater<int> >
+#define mod             1000000007
+#define inf             1e18
+#define f(i,n)          for(int i=0;i<n;i++)
+#define w(x)            int x; cin>>x; while(x--)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define PI              3.1415926535
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n, m;
+	cin >> n >> m;
+	int a[n];
+	f(i, n)
+	{
+		cin >> a[i];
+	}
+	vi req;
+	f(i, n)
+	{
+		int y = a[i];
+		vi v;
+		f(j, i)
+		{
+			v.pb(a[j]);
+		}
+		sort(v.begin(), v.end());
+		int cnt = v.size();
+
+		f(j, i)
+		{
+
+			if (v[j] + y <= m)
+			{
+				y += v[j];
+				cnt--;
+			}
+		}
+		req.pb(cnt);
+	}
+	f(i, n)
+	cout << req[i] << " ";
+	cout << endl;
+
+
+
+
+
+
+
+
+}
+ 
