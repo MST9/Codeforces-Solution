fba2d8450cabcd52560ecf9c346958c483f1f940
MOHIT SINGH TOMAR <mohitsinghtomar9797@gmail.com>
2020-08-17 00:37:54 +0530

    Create 1392B. Omkar and Infinity Clock.cpp

---
+++ 1392B. Omkar and Infinity Clock.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ff              first
+#define ss              second
+#define int             long long int
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int,vi,greater<int> >
+#define mod             1000000007
+#define inf             1e18
+#define f(i,n)          for(int i=0;i<n;i++)
+#define w(x)            int x; cin>>x; while(x--)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define PI              3.1415926535
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+	w(t)
+	{
+		int n, k;
+		cin >> n >> k;
+		int a[n];
+		int dw = -inf;
+		f(i, n)
+		{
+			cin >> a[i];
+			if (a[i] > dw)
+				dw = a[i];
+
+		}
+		vi p, q;
+		int d = -inf;
+		f(i, n)
+		{
+			int u = dw - a[i];
+			p.pb(u);
+		}
+		f(i, n)
+		{
+			if (p[i] > d)
+			{
+				d = p[i];
+			}
+		}
+		f(i, n)
+		{
+			int u = d - p[i];
+			q.pb(u);
+		}
+
+		if (k % 2 == 0)
+		{
+			f(i, n)
+			{
+				cout << q[i] << " ";
+			}
+			cout << endl;
+		}
+		else
+		{
+			f(i, n)
+			{
+				cout << p[i] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+
+
+
+
+
+
+
+}
+ 
